c929fab33f225e711aaac20d733f54ff1d2fbb97
Ebin Soby <mailebinsoby@gmail.com>
2022-01-05 13:31:57 -0500

    Basic Layout

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,8 @@
+// var options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//   headers: {
+//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//     'x-rapidapi-key': 'a235a829b4mshdc70fd08ba979cdp1c66f5jsnb285d3f378a9'
+//   }
+// };
